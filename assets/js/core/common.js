2b357c0dd376705e3a8277a6888ec813b96e1840
sheyda-dashtizadeh <sheida.dashtizadeh@gmail.com>
2022-01-17 02:56:28 +0330

    last edit

--- assets/js/core/common.js
+++ assets/js/core/common.js
@@ -9,3 +9,26 @@ window.addEventListener("scroll", () => {
     const nav = document.querySelector(".navbar");
     nav.classList.toggle('scrolled', window.scrollY > 1);
 });
+
+// Active Navbar Item on scroll
+const sections = document.querySelectorAll("section[id]");
+console.log(sections);
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+    let scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 10;
+        sectionId = current.getAttribute("id");
+
+        if (
+            scrollY > sectionTop &&
+            scrollY <= sectionTop + sectionHeight
+        ) {
+            document.querySelector(".navbar-nav a[href*=" + sectionId + "].nav-link").classList.add("active");
+        } else {
+            document.querySelector(".navbar-nav a[href*=" + sectionId + "].nav-link").classList.remove("active");
+        }
+    });
+}
