c7d80e681101f594fd5687e4ad2d17db0180a8df
Sheyda Dashtizadeh <sheida.dashtizadeh@gmail.com>
2022-01-25 23:23:04 +0330

    remove cart js & add number input spinner js

--- assets/js/core/control-item.js
+++ assets/js/core/control-item.js
@@ -1,48 +1,12 @@
-/* 
-$('.movie-cover').scrollFix({
-    side: 'top',
-    topPosition: 0,
-});
-*/
-// copy => movie_info(comment), admin dashboard (comment, ticekts)
-var cartOpen = false;
-var numberOfProducts = 0;
+import QuantityInput from '../plugins/quantity.js';
 
-$('body').on('click', '.js-toggle-cart', toggleCart);
-$('body').on('click', '.js-add-product', addProduct);
-$('body').on('click', '.js-remove-product', removeProduct);
+// Set up quantity forms
+(function () {
+    let quantities = document.querySelectorAll('[data-quantity]');
 
-function toggleCart(e) {
-    e.preventDefault();
-    cartOpen ? closeCart() : openCart();
-}
-
-function openCart() {
-    cartOpen = true;
-    $('body').addClass('open');
-}
-
-function closeCart() {
-    cartOpen = false;
-    $('body').removeClass('open');
-}
-
-function addProduct(e) {
-    e.preventDefault();
-    openCart();
-    $('.js-cart-empty').addClass('hide');
-    var product = $('.js-cart-product-template').html();
-    $('.js-cart-products').prepend(product);
-    numberOfProducts++;
-}
-
-function removeProduct(e) {
-    e.preventDefault();
-    console.log('click');
-    // $(this).closest('.js-cart-product').hide(250);
-    $(this).closest('.js-cart-product').addClass(".hide-product");
-    numberOfProducts--;
-    if (numberOfProducts == 0) {
-        $('.js-cart-empty').removeClass('hide');
+    if (quantities instanceof Node) quantities = [quantities];
+    if (quantities instanceof NodeList) quantities = [].slice.call(quantities);
+    if (quantities instanceof Array) {
+        quantities.forEach(div => (div.quantity = new QuantityInput(div, 'Down', 'Up')));
     }
-}
+})();
